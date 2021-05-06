27939701b7e2e6a65f69a0d5c1173afb21e0f15f
Filipe Kiss <hello@filipekiss.com.br>
2021-05-06 18:55:59 +0200

    feat: add search button and input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./Search.css";
 
 /* Feel free to make this a functional component if you consider it necessary */
 class Search extends React.Component {
   render() {
     return (
-      <div>
-        Edit <code>src/components/Search.js</code>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="What are you looking for?"
+        />
+        <button className="search-button" type="search">
+          Search
+        </button>
       </div>
     );
   }
