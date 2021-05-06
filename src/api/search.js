5314badfcc230db6542a7425768480192587bd26
Filipe Kiss <hello@filipekiss.com.br>
2021-05-06 19:50:33 +0200

    feat: add api fetcher

--- src/api/search.js
+++ src/api/search.js
@@ -1,3 +1,23 @@
 export function fetchSearchResults(query) {
-  // Write your code here
+  const hasQueryTerm = query !== "" && query !== undefined;
+  if (typeof query !== "string" && hasQueryTerm) {
+    throw new TypeError(
+      `TypeError: Expected query to be a string, instead got "${typeof query}"`
+    );
+  }
+  if (!Boolean(query)) {
+    // No query or empty query
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  }
+  try {
+    return fetch(makeSearchUrl(query)).then((results) => results.json());
+  } catch (e) {
+    throw new Error("Something went wrong");
+  }
 }
+
+const API_URL = "http://localhost:3001/products";
+
+const makeSearchUrl = (query) => `${API_URL}?q=${encodeURIComponent(query)}`;
