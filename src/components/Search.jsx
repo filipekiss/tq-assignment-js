1b597918681bc8817344afe2c2b744608f59bc74
Filipe Kiss <hello@filipekiss.com.br>
2021-05-06 23:11:17 +0200

    feat: add styling to search results

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,25 @@
-import React, { useReducer, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { fetchSearchResults } from "../api/search";
 import "./Search.css";
+
+function formatPrice(price) {
+  return `€ ${price.toFixed(2)}`;
+}
 
 function Search(props) {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState("");
 
+  useEffect(() => {
+    fetchSearchResults("Museums").then(setResults);
+  }, []);
+
   return (
     <div>
-      <div className="search-container">
+      <div className="search-form">
         <input
           type="text"
-          className="search-input"
+          className="text-input"
           placeholder="What are you looking for?"
           onChange={(e) => setQuery(e.target.value)}
           onKeyDown={(e) => {
@@ -21,7 +29,7 @@ function Search(props) {
           }}
         />
         <button
-          className="search-button"
+          className="action-button"
           type="search"
           onClick={() => fetchSearchResults(query).then(setResults)}
         >
@@ -30,10 +38,15 @@ function Search(props) {
       </div>
       <div className="search-results">
         {Boolean(results) &&
-          results.map((result) => (
-            <div className="search-result-item">
-              {JSON.stringify({ type: "result", result })}
-            </div>
+          results.map((result, index) => (
+            <a className="card media" key={index} href={result.productUrl}>
+              <img alt={result.title} src={result.image} />
+              <div className="content">
+                <h2 className="title">{result.title}</h2>
+                <p className="summary">{result.summary}</p>
+                <div className="price-tag">{formatPrice(result.price)}</div>
+              </div>
+            </a>
           ))}
       </div>
     </div>
